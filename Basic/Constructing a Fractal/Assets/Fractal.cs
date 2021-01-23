39c4c421a6375c1e8f646329c58a51c15190a756
NTMRWSB <wzx20020111@outlook.com>
2021-01-23 16:41:14 +0800

    2021/1/25 Basic Old Update

---
+++ Basic/Constructing a Fractal/Assets/Fractal.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    public Mesh mesh;
+    public Material material;
+
+    public int maxDepth;
+
+    public float childScale;
+
+    private int depth;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        if (depth < maxDepth)
+        {
+            StartCoroutine(CreateChildren());
+        }
+    }
+
+    private void Initialize(Fractal parent, Vector3 direction, Quaternion orientation)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        childScale = parent.childScale;
+        transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = direction * (0.5f + 0.5f * childScale);
+        transform.localRotation = orientation;
+    }
+
+    private IEnumerator CreateChildren()
+    {
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").AddComponent<Fractal>().
+                Initialize(this, Vector3.up, Quaternion.identity);
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").AddComponent<Fractal>().
+            Initialize(this, Vector3.right, Quaternion.Euler(0f, 0f, -90f));
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").AddComponent<Fractal>().
+            Initialize(this, Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
